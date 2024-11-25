24a95e625d2c909d13b576fb7e72fae46e88162c
Lifailon <deadepicboss@gmail.com>
2024-11-25 17:44:04 +0300

    update build for different platforms

--- build.sh
+++ build.sh
@@ -7,9 +7,15 @@ rm -rf bin/*
 architectures=(amd64 arm64)
 for arch in "${architectures[@]}"; do
     GOOS=linux GOARCH=$arch go build -o bin/lazyjournal-$version-linux-$arch
+    GOOS=darwin GOARCH=$arch go build -o bin/lazyjournal-$version-macos-$arch
+    if [[ $arch == "amd64" ]]; then
+        GOOS=windows GOARCH=$arch go build -o bin/lazyjournal-$version-windows-$arch
+    fi
 done
 
 if [[ -n "$2" ]]; then
     snapcraft --destructive-mode
     mv "$(ls *.snap)" "bin/$(ls *.snap | sed "s/_/-/g")"
 fi
+
+ls -lh bin
