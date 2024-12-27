d5e6515f47a7311626abfd6a7f76977a616d7ec9
Lifailon <deadepicboss@gmail.com>
2024-12-27 10:59:09 +0300

    update build and install for openbsd

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-version=${1:-"0.6.0"}
+version=${1:-"0.7.0"}
 mkdir -p bin
 rm -rf bin/*
 
 go get -u golang.org/x/sys
 
-if ! command -v golangci-lint &> /dev/null; then
+golangci_lint_version=$(golangci-lint --version 2> /dev/null)
+if [ -z "$golangci_lint_version" ]; then
     go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-else
-    golangci-lint --version
 fi
 
 output=$(golangci-lint run ./... --build-tags=buildvcs=false)
@@ -19,6 +18,7 @@ if [ -z "$output" ]; then
     for arch in "${architectures[@]}"; do
         GOOS=linux GOARCH=$arch go build -o bin/lazyjournal-$version-linux-$arch
         GOOS=darwin GOARCH=$arch go build -o bin/lazyjournal-$version-darwin-$arch
+        GOOS=openbsd GOARCH=$arch go build -o bin/lazyjournal-$version-openbsd-$arch
         GOOS=windows GOARCH=$arch go build -o bin/lazyjournal-$version-windows-$arch.exe
     done
     ls -lh bin
--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ esac
 
 case "$SHELL" in
     */bash) shellRc="$HOME/.bashrc" ;;
-    */zsh) shellRc="$HOME/.zshrc" ;;
+    */zsh) shellRc="$HOME/.zshrc" ;; # MacOS
+    */ksh) shellRc="$HOME/.kshrc" ;; # OpenBSD
     *)
-        echo -e "\033[31mError.\033[0m Shell not supported: $SHELL"
-        echo -e "Create a request with a \033[31mproblem\033[0m: https://github.com/Lifailon/lazyjournal/issues"
-        exit 1
+        shellRc="$HOME/.profile"
+        echo -e "Shell not supported: $SHELL, profile is used"
         ;;
 esac
 
@@ -28,16 +28,16 @@ mkdir -p $HOME/.local/bin
 
 grep -F 'export PATH=$PATH:$HOME/.local/bin' $shellRc > /dev/null || { 
     echo 'export PATH=$PATH:$HOME/.local/bin' >> $shellRc
-    source $shellRc
+    source $shellRc 2> /dev/null || . $shellRc
 }
 
-GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/Lifailon/lazyjournal/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_LATEST_VERSION=$(curl -L -sS -H 'Accept: application/json' https://github.com/Lifailon/lazyjournal/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 if [ -z "$GITHUB_LATEST_VERSION" ]; then
     echo -e "\033[31mError.\033[0m Unable to get the latest version from GitHub repository, check your internet connection."
     exit 1
 else
     BIN_URL="https://github.com/Lifailon/lazyjournal/releases/download/$GITHUB_LATEST_VERSION/lazyjournal-$GITHUB_LATEST_VERSION-$OS-$ARCH"
-    curl -L -s "$BIN_URL" -o $HOME/.local/bin/lazyjournal
+    curl -L -sS "$BIN_URL" -o $HOME/.local/bin/lazyjournal
     chmod +x $HOME/.local/bin/lazyjournal
     if [ $OS = "darwin" ]; then
         xattr -d com.apple.quarantine $HOME/.local/bin/lazyjournal
