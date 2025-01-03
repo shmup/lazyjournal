af90f65aca2665a736f14120c61fb705bfe64380
Lifailon <deadepicboss@gmail.com>
2025-01-04 02:34:14 +0300

    add check linters via gocritic

--- build.sh
+++ build.sh
@@ -6,14 +6,24 @@ rm -rf bin/*
 
 go mod tidy
 
-golangci_lint_version=$(golangci-lint --version 2> /dev/null)
-if [ -z "$golangci_lint_version" ]; then
+golangci_version=$(golangci-lint --version 2> /dev/null)
+if [ -z "$golangci_version" ]; then
     go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 fi
 
-output=$(golangci-lint run ./... --build-tags=buildvcs=false)
+gocritic_version=$(gocritic --version 2> /dev/null)
+if [ -z "$gocritic_version" ]; then
+    go install -v github.com/go-critic/go-critic/cmd/gocritic@latest
+fi
 
-if [ -z "$output" ]; then
+golangci_check=$(golangci-lint run ./... --build-tags=buildvcs=false)
+gocritic_check=$(gocritic check -enableAll ./...)
+
+if [ -n "$golangci_check" ]; then
+    echo "$golangci_check"
+elif [ -n "$gocritic_check" ]; then
+    echo "$gocritic_check"
+else
     architectures=(amd64 arm64)
     for arch in "${architectures[@]}"; do
         GOOS=linux GOARCH=$arch go build -o bin/lazyjournal-$version-linux-$arch
@@ -23,6 +33,4 @@ if [ -z "$output" ]; then
         GOOS=windows GOARCH=$arch go build -o bin/lazyjournal-$version-windows-$arch.exe
     done
     ls -lh bin
-else
-    echo "$output"
 fi
