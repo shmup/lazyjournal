da73c8ca1901965856f54c8531a9a596b2a614c3
Lifailon <deadepicboss@gmail.com>
2025-02-22 12:48:10 +0300

    add tests for unix files and journald

--- main_test.go
+++ main_test.go
@@ -12,9 +12,9 @@ func TestWinFiles(t *testing.T) {
 		name       string
 		selectPath string
 	}{
-		// {"Program Files", "ProgramFiles"},
+		{"Program Files", "ProgramFiles"},
 		{"Program Files 86", "ProgramFiles86"},
-		// {"ProgramData", "ProgramData"},
+		{"ProgramData", "ProgramData"},
 		// {"AppData/Local", "AppDataLocal"},
 		// {"AppData/Roaming", "AppDataRoaming"},
 	}
@@ -22,10 +22,10 @@ func TestWinFiles(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			app := &App{
 				testMode:     true,
+				logViewCount: "100000",
+				getOS:        "windows",
 				systemDisk:   "C",
 				userName:     "lifailon",
-				getOS:        "windows",
-				logViewCount: "100000",
 				selectPath:   tc.selectPath,
 			}
 
@@ -53,6 +53,82 @@ func TestWinFiles(t *testing.T) {
 	}
 }
 
+func TestUnixFiles(t *testing.T) {
+	testCases := []struct {
+		name       string
+		selectPath string
+	}{
+		{"Var Log", "/var/log/"},
+		{"Home", "/home/"},
+		{"Descriptors", "descriptor"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			app := &App{
+				testMode:     true,
+				logViewCount: "100000",
+				getOS:        "linux",
+				userName:     "lifailon",
+				selectPath:   tc.selectPath,
+			}
+
+			app.loadFiles(app.selectPath)
+			if len(app.logfiles) == 0 {
+				t.Errorf("File list is null")
+			} else {
+				t.Log("Log files count:", len(app.logfiles))
+			}
+
+			var ansiEscape = regexp.MustCompile(`\x1b\[[0-9;]*m`)
+			for _, logfile := range app.logfiles {
+				logFileName := ansiEscape.ReplaceAllString(logfile.name, "")
+				startTime := time.Now()
+				app.loadFileLogs(strings.TrimSpace(logFileName), true)
+				endTime := time.Since(startTime)
+				t.Log("Path:", app.lastLogPath, ">>> LINE:\x1b[0;33m", len(app.currentLogLines), "\x1b[0;0m& TIME:\x1b[0;33m", endTime, "\x1b[0;0m")
+			}
+		})
+	}
+}
+
+func TestJournal(t *testing.T) {
+	testCases := []struct {
+		name        string
+		journalName string
+	}{
+		{"Services", "services"},
+		{"Units", "UNIT"},
+		{"User units", "USER_UNIT"},
+		{"Kernel", "kernel"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			app := &App{
+				testMode:     true,
+				logViewCount: "100000",
+				getOS:        "linux",
+				selectUnits:  tc.journalName,
+			}
+
+			app.loadServices(app.selectUnits)
+			if len(app.journals) == 0 {
+				t.Errorf("File list is null")
+			} else {
+				t.Log("Journal count:", len(app.journals))
+			}
+
+			var ansiEscape = regexp.MustCompile(`\x1b\[[0-9;]*m`)
+			for _, journal := range app.journals {
+				serviceName := ansiEscape.ReplaceAllString(journal.name, "")
+				startTime := time.Now()
+				app.loadJournalLogs(strings.TrimSpace(serviceName), true)
+				endTime := time.Since(startTime)
+				t.Log("Journal:", app.lastLogPath, ">>> LINE:\x1b[0;33m", len(app.currentLogLines), "\x1b[0;0m& TIME:\x1b[0;33m", endTime, "\x1b[0;0m")
+			}
+		})
+	}
+}
+
 func TestDockerContainer(t *testing.T) {
 	testCases := []struct {
 		name                         string
@@ -66,9 +142,10 @@ func TestDockerContainer(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			app := &App{
 				testMode:                     true,
-				selectContainerizationSystem: tc.selectContainerizationSystem,
 				logViewCount:                 "100000",
+				selectContainerizationSystem: tc.selectContainerizationSystem,
 			}
+
 			app.loadDockerContainer(app.selectContainerizationSystem)
 			if len(app.dockerContainers) == 0 {
 				t.Errorf("Container list is null")
